04a8e72d144574be740059907928008f3edeb8c9
Carlos <coalva.94@gmail.com>
2024-06-20 14:27:18 -0500

    footer and navbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import indexRouter from './routes/index.js';
+import path from 'path';
 
 const app = express();
 
@@ -9,6 +10,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(indexRouter);
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.listen(3000, () => {
   console.log("Server is running on port", 3000);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,5 @@ const router = Router();
 
 router.get('/', (req, res) => res.render('index', {title: 'TecShop | Home'}));
 router.get('/about', (req, res) => res.render('about', {title: 'About me'}));
-
+router.get('/products', (req, res) => res.render('products', {title: 'products'}));
 export default router;
